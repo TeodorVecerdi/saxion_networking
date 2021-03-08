e96012b21a8e9676f4724921cac522a41dfa553d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-08 19:00:54 +0200

    Update Logger formatting

--- Assignment 2/server/Logger.cs
+++ Assignment 2/server/Logger.cs
@@ -16,59 +16,33 @@ public static class Logger {
         return message is IFormattable formattable ? formattable.ToString(null, CultureInfo.InvariantCulture) : message.ToString();
     }
 
-    public static void Except(Exception exception, bool includeStackTrace = false) {
+    public static void Except(Exception exception, bool includeStackTrace = false, bool includeFileInfo = false, bool includeTimestamp = true) {
         var message = exception.Message;
         if (includeStackTrace) message += $"\n{exception.StackTrace}";
-        Error(message, "EXCEPTION");
+        Error(message, "EXCEPTION", includeFileInfo, includeTimestamp);
     }
 
-    public static void Info(object message, string messageTitle = "INFO") {
+    public static void Info(object message, string messageTitle = "INFO", bool includeFileInfo = false, bool includeTimestamp = true) {
 #if DEBUG
-        var stack = new StackFrame(1, true);
-        var mth = stack.GetMethod();
-        var fname = stack.GetFileName();
-        var lineNumber = stack.GetFileLineNumber();
-        var fileName = fname?.Substring(fname.LastIndexOf("\\", StringComparison.InvariantCulture) + 1);
-        var className = mth.ReflectedType?.Name;
-        var method = new StringBuilder();
-        method.Append(mth.Name);
-        method.Append("(");
-        var methodParameters = mth.GetParameters();
-        for (var i = 0; i < methodParameters.Length; i++) {
-            method.Append(methodParameters[i].ParameterType);
-            if (i != methodParameters.Length - 1) method.Append(", ");
-        }
+        var fileInfo = includeFileInfo ? GetFileInfo() : "";
+        messageTitle = includeTimestamp ? GetTitleWithDate(messageTitle) : messageTitle;
 
-        method.Append(")");
         Console.ForegroundColor = ConsoleColor.White;
         Console.BackgroundColor = ConsoleColor.Blue;
         Console.Write("[" + messageTitle + "]");
         Console.ResetColor();
         Console.Write(" " + GetString(message));
         Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine(logFormat, className, method, lineNumber, fileName);
+        Console.WriteLine(fileInfo);
         Console.ResetColor();
 #endif
     }
 
-    public static void Warn(object message, string messageTitle = "WARN") {
+    public static void Warn(object message, string messageTitle = "WARN", bool includeFileInfo = false, bool includeTimestamp = true) {
 #if DEBUG
-        var stack = new StackFrame(1, true);
-        var mth = stack.GetMethod();
-        var fname = stack.GetFileName();
-        var lineNumber = stack.GetFileLineNumber();
-        var fileName = fname?.Substring(fname.LastIndexOf("\\", StringComparison.InvariantCulture) + 1);
-        var className = mth.ReflectedType?.Name;
-        var method = new StringBuilder();
-        method.Append(mth.Name);
-        method.Append("(");
-        var methodParameters = mth.GetParameters();
-        for (var i = 0; i < methodParameters.Length; i++) {
-            method.Append(methodParameters[i].ParameterType);
-            if (i != methodParameters.Length - 1) method.Append(", ");
-        }
+        var fileInfo = includeFileInfo ? GetFileInfo() : "";
+        messageTitle = includeTimestamp ? GetTitleWithDate(messageTitle) : messageTitle;
 
-        method.Append(")");
         Console.ForegroundColor = ConsoleColor.White;
         Console.BackgroundColor = ConsoleColor.DarkYellow;
         Console.Write("[" + messageTitle + "]");
@@ -76,14 +50,34 @@ public static class Logger {
         Console.ForegroundColor = ConsoleColor.DarkYellow;
         Console.Write(" " + GetString(message));
         Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine(logFormat, className, method, lineNumber, fileName);
+        Console.WriteLine(fileInfo);
         Console.ResetColor();
 #endif
     }
 
-    public static void Error(object message, string messageTitle = "ERROR") {
+    public static void Error(object message, string messageTitle = "ERROR", bool includeFileInfo = false, bool includeTimestamp = true) {
 #if DEBUG
-        var stack = new StackFrame(1, true);
+        var fileInfo = includeFileInfo ? GetFileInfo() : "";
+        messageTitle = includeTimestamp ? GetTitleWithDate(messageTitle) : messageTitle;
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.Write("[" + messageTitle + "]");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write(" " + GetString(message));
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine(fileInfo);
+        Console.ResetColor();
+#endif
+    }
+
+    private static string GetTitleWithDate(string messageTitle) {
+        return $"{DateTime.Now:HH:mm:ss dd/mm/yyyy} - {messageTitle}";
+    }
+
+    private static string GetFileInfo() {
+        var stack = new StackFrame(2, true);
         var mth = stack.GetMethod();
         var fname = stack.GetFileName();
         var lineNumber = stack.GetFileLineNumber();
@@ -99,15 +93,7 @@ public static class Logger {
         }
 
         method.Append(")");
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.BackgroundColor = ConsoleColor.DarkRed;
-        Console.Write("[" + messageTitle + "]");
-        Console.ResetColor();
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.Write(" " + GetString(message));
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine(logFormat, className, method, lineNumber, fileName);
-        Console.ResetColor();
-#endif
+        var fileInfo = string.Format(logFormat, className, method, lineNumber, fileName);
+        return fileInfo;
     }
 }
