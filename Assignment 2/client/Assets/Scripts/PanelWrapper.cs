d56def8db89ad0425345efd014c90cd931ca29ee
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-08 15:04:18 +0200

    Add Unity project

---
+++ Assignment 2/client/Assets/Scripts/PanelWrapper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Wrapper around the chat view.
+ * 
+ * Methods of interest:
+ *  OnChatTextEntered += Action<String>
+ *  AddOutput(string)
+ *  ClearInput()
+ *  
+ *  @author J.C. Wichman
+ */
+public class PanelWrapper : MonoBehaviour
+{
+    //hook up these fields to the correct components in the view
+    [SerializeField] private InputField _inputFieldChat = null;
+    [SerializeField] private Text _textOutput = null;
+    [SerializeField] private ScrollRect _scrollRect = null;
+
+    private bool _focusedRequested = false;
+    private bool _scrollRequested = false;
+
+    public event Action<string> OnChatTextEntered = delegate { };
+
+    private void Start()
+    {
+        //setup chat input listener to trigger on enter
+        _inputFieldChat.onEndEdit.AddListener(
+            (value) => {
+                if (Input.GetKeyDown(KeyCode.Return)) OnChatTextEntered(value);
+            }
+         );
+    }
+
+    public void ClearInput()
+    {
+        _inputFieldChat.text = "";
+        _focusedRequested = true;
+    }
+
+    public void ClearOutput()
+    {
+        _textOutput.text = "";
+        _scrollRequested = true;
+    }
+
+    public void AddOutput(string pOutput)
+    {
+        _textOutput.text += pOutput + "\n";
+        _scrollRequested = true;
+    }
+
+    private void Update()
+    {
+        checkFocus();
+    }
+
+    private void checkFocus()
+    {
+        if (_focusedRequested)
+        {
+            _inputFieldChat.ActivateInputField();
+            _inputFieldChat.Select();
+            _focusedRequested = false;
+        }
+
+        if (_scrollRequested)
+        {
+            _scrollRect.verticalNormalizedPosition = 0;
+            _scrollRequested = false;
+        }
+    }
+
+}
---
+++ Assignment 2/client/Assets/Scripts/TCPChatClient.cs
@@ -0,0 +1,68 @@
+﻿using shared;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+/**
+ * Assignment 2 - Starting project.
+ * 
+ * @author J.C. Wichman
+ */
+public class TCPChatClient : MonoBehaviour
+{
+    [SerializeField] private PanelWrapper _panelWrapper = null;
+    [SerializeField] private string _hostname = "localhost";
+    [SerializeField] private int _port = 55555;
+
+    private TcpClient _client;
+
+    void Start()
+    {
+        _panelWrapper.OnChatTextEntered += onTextEntered;
+        connectToServer();
+    }
+
+    private void connectToServer()
+    {
+        try
+        {
+			_client = new TcpClient();
+            _client.Connect(_hostname, _port);
+            _panelWrapper.ClearOutput();
+            _panelWrapper.AddOutput("Connected to server.");
+        }
+        catch (Exception e)
+        {
+            _panelWrapper.AddOutput("Could not connect to server:");
+            _panelWrapper.AddOutput(e.Message);
+        }
+    }
+
+    private void onTextEntered(string pInput)
+    {
+        if (pInput == null || pInput.Length == 0) return;
+
+        _panelWrapper.ClearInput();
+
+		try 
+        {
+			//echo client - send one, expect one (hint: that is not how a chat works ...)
+			byte[] outBytes = Encoding.UTF8.GetBytes(pInput);
+			StreamUtil.Write(_client.GetStream(), outBytes);
+
+			byte[] inBytes = StreamUtil.Read(_client.GetStream());
+            string inString = Encoding.UTF8.GetString(inBytes);
+            _panelWrapper.AddOutput(inString);
+		} 
+        catch (Exception e) 
+        {
+            _panelWrapper.AddOutput(e.Message);
+			//for quicker testing, we reconnect if something goes wrong.
+			_client.Close();
+			connectToServer();
+		}
+    }
+
+}
+
