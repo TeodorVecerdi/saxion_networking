36edbeb49c953f0fd1fa77882d729a8d8876d14e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-08 16:56:13 +0200

    Implement client

---
+++ Assignment 2/client/Assets/Scripts/ServerUtility.cs
@@ -0,0 +1,11 @@
+﻿public static class ServerUtility {
+    public static string EncodeMessage(string rawMessage) {
+        if (rawMessage.StartsWith("/") || string.Equals(rawMessage, "HEARTBEAT")) return rawMessage;
+        return $"MSG:{rawMessage}";
+    }
+
+    public static byte[] EncodeMessageAsBytes(string rawMessage) {
+        var message = EncodeMessage(rawMessage);
+        return System.Text.Encoding.UTF8.GetBytes(message);
+    }
+}
--- Assignment 2/client/Assets/Scripts/TCPChatClient.cs
+++ Assignment 2/client/Assets/Scripts/TCPChatClient.cs
@@ -9,60 +9,92 @@ using UnityEngine;
  * 
  * @author J.C. Wichman
  */
-public class TCPChatClient : MonoBehaviour
-{
-    [SerializeField] private PanelWrapper _panelWrapper = null;
-    [SerializeField] private string _hostname = "localhost";
-    [SerializeField] private int _port = 55555;
+public class TCPChatClient : MonoBehaviour {
+    [SerializeField] private PanelWrapper panelWrapper = null;
+    [SerializeField] private string hostname = "localhost";
+    [SerializeField] private int port = 55555;
 
-    private TcpClient _client;
+    private DateTime lastHeartbeatTime;
+    private float? serverTimeout;
+    private TcpClient client;
 
-    void Start()
-    {
-        _panelWrapper.OnChatTextEntered += onTextEntered;
-        connectToServer();
+    private void Start() {
+        panelWrapper.OnChatTextEntered += OnTextEntered;
+        ConnectToServer();
     }
 
-    private void connectToServer()
-    {
-        try
-        {
-			_client = new TcpClient();
-            _client.Connect(_hostname, _port);
-            _panelWrapper.ClearOutput();
-            _panelWrapper.AddOutput("Connected to server.");
+    private void Update() {
+        try {
+            if (client.Available > 0) {
+                var stream = client.GetStream();
+                var inBytes = StreamUtil.Read(stream);
+                var received = Encoding.UTF8.GetString(inBytes);
+                ProcessMessage(received);
+            }
+        } catch (Exception e) {
+            panelWrapper.AddOutput(e.Message);
         }
-        catch (Exception e)
-        {
-            _panelWrapper.AddOutput("Could not connect to server:");
-            _panelWrapper.AddOutput(e.Message);
+        
+        var currentTime = DateTime.Now;
+        if (serverTimeout.HasValue && (currentTime - lastHeartbeatTime).Seconds > serverTimeout.Value / 2.0f) {
+            EmitHeartbeat();
         }
     }
 
-    private void onTextEntered(string pInput)
-    {
-        if (pInput == null || pInput.Length == 0) return;
-
-        _panelWrapper.ClearInput();
-
-		try 
-        {
-			//echo client - send one, expect one (hint: that is not how a chat works ...)
-			byte[] outBytes = Encoding.UTF8.GetBytes(pInput);
-			StreamUtil.Write(_client.GetStream(), outBytes);
-
-			byte[] inBytes = StreamUtil.Read(_client.GetStream());
-            string inString = Encoding.UTF8.GetString(inBytes);
-            _panelWrapper.AddOutput(inString);
-		} 
-        catch (Exception e) 
-        {
-            _panelWrapper.AddOutput(e.Message);
-			//for quicker testing, we reconnect if something goes wrong.
-			_client.Close();
-			connectToServer();
-		}
+    private void EmitHeartbeat() {
+        EmitMessage("HEARTBEAT");
     }
 
+    private void EmitMessage(string message) {
+        try {
+            var outBytes = ServerUtility.EncodeMessageAsBytes(message);
+            lastHeartbeatTime = DateTime.Now;
+            Debug.Log($"Sent message: {ServerUtility.EncodeMessage(message)}");
+            StreamUtil.Write(client.GetStream(), outBytes);
+        } catch (Exception e) {
+            panelWrapper.AddOutput(e.Message);
+            //for quicker testing, we reconnect if something goes wrong.
+            client.Close();
+            ConnectToServer();
+        }
+    }
+
+    private void ProcessMessage(string message) {
+        // Normal message to output to console
+        if (message.StartsWith("MSG:")) {
+            Debug.Log($"Received message: {message}");
+            panelWrapper.AddOutput(message.Substring(4));
+            return;
+        }
+
+        // Server timeout setup for heartbeat
+        if (message.StartsWith("TIMEOUT:")) {
+            var timeoutStr = message.Split(':')[1];
+            if (!float.TryParse(timeoutStr, out var timeout)) {
+                serverTimeout = 0.5f;
+                return;
+            }
+            
+            serverTimeout = timeout;
+            Debug.Log($"Received timeout from server {serverTimeout}");
+        }
+    }
+
+    private void OnTextEntered(string input) {
+        if (string.IsNullOrEmpty(input)) return;
+        panelWrapper.ClearInput();
+        EmitMessage(input);
+    }
+
+    private void ConnectToServer() {
+        try {
+            client = new TcpClient();
+            client.Connect(hostname, port);
+            panelWrapper.ClearOutput();
+            panelWrapper.AddOutput("Connected to server.");
+        } catch (Exception e) {
+            panelWrapper.AddOutput("Could not connect to server:");
+            panelWrapper.AddOutput(e.Message);
+        }
+    }
 }
-
