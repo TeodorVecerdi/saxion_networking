daaae0aa60166bfe5879c9cea58e5638b4af15f7
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-27 19:13:13 +0200

    Add exception type to Log.Except

--- Assignment4/SerializationSystem/log/Log.cs
+++ Assignment4/SerializationSystem/log/Log.cs
@@ -22,7 +22,7 @@ namespace SerializationSystem.Logging {
         [Conditional("DEBUG")]
         public static void Except(Exception exception, object ctx = null, string messageTitle = "EXCEPTION", bool includeStackTrace = kIncludeStackTrace, bool includeFileInfo = kIncludeFileInfo, bool includeTimestamp = kIncludeTimestamp) {
             if (stackJump == -1) stackJump = 4;
-            var message = exception.Message;
+            var message = $"{exception.GetType().FullName}: {exception.Message}";
             if (includeStackTrace) message += $"\n{exception.StackTrace}";
             Error(message, ctx, messageTitle, includeFileInfo, includeTimestamp);
         }
